6cd3e365bc7b75f83af1ba408aee70d54b611011
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-09-20 08:46:22 +0530

    filter

--- src/component/HotelCityCard.js
+++ src/component/HotelCityCard.js
@@ -21,16 +21,50 @@ const HotelCityCard = () => {
     dispatch(addCityHotel(filteredHotels));
   };
 
+  const handleFacilityChange = (facility_id) => {
+    const filterLogic = cityHotel.filter((hotel) =>
+      Object.values(hotel.facilityIds).includes(facility_id)
+    );
+    dispatch(addCityHotel(filterLogic));
+  };
+
   return (
     <>
       <div className=" flex flex-row gap-10 bg-[#1a1e21]">
-        <div className=" w-[450px] border-2 border-white h-[100vh] overflow-y-auto">
-          <button
-            className="px-3 py-2 bg-red-500 rounded-xl"
-            onClick={handleFilterTopRated}
-          >
-            Top Rated Hotel Stays
-          </button>
+        <div className=" w-[450px] border-2 border-white h-[100vh] overflow-y-auto py-3">
+          {facility?.length > 0 ? (
+            facility
+              // .filter((_, index) => index !== 3) // Skip the 4th element (index 3)
+              // .slice(0, 15)
+              .map((item, index) => (
+                <div key={index} className="flex px-2 gap-2">
+                  <input
+                    type="checkbox"
+                    id={`facility-${item.facility_id}`}
+                    name="facility"
+                    value={item.facility_id}
+                    className="mt-1"
+                    onChange={() => handleFacilityChange(item.facility_id)}
+                  />
+                  <label
+                    htmlFor={`facility-${item.facility_id}`}
+                    className="text-white"
+                  >
+                    {item.facility}
+                  </label>
+                </div>
+              ))
+          ) : (
+            <p className="text-white">No facilities available</p>
+          )}
+          {
+            <button
+              className="px-3 py-2 bg-red-500 rounded-xl"
+              onClick={handleFilterTopRated}
+            >
+              Top Rated Hotel Stays
+            </button>
+          }
         </div>
         <div className=" flex flex-col w-full h-[100vh] overflow-y-auto border border-gray-300 overscroll-auto scrollbar-hide px-10 py-5 rounded-lg">
           {cityHotel && cityHotel.length > 0 ? (
@@ -43,6 +77,9 @@ const HotelCityCard = () => {
             </div>
           ) : (
             <div className=" flex flex-wrap gap-10 mb-6 flex-col">
+              <div>
+                <p className="text-white">No Hotels available For This Filters</p>
+              </div>
               <NewSkeletonContainer />
               <NewSkeletonContainer />
               <NewSkeletonContainer />
