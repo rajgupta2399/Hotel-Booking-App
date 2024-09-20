8443998ca6dd14a2f3ba707796cd61f3a47dacad
rajgupta2399 <rajumardotnet2499@gmail.com>
2024-09-20 17:48:15 +0530

    newHotelDetailSlice

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import countryHotelReducer from "./countrySlice";
 import hotelFacilityReducer from "./facilitySlice";
 import cityHotelReducer from "./cityHotelSlice";
+import hotelDetailReducer from "./hotelDetailSlice";
 
 const store = configureStore({
   reducer: {
     country: countryHotelReducer,
     facility: hotelFacilityReducer,
     cityHotel: cityHotelReducer,
+    hotelDetail: hotelDetailReducer,
   },
 });
 
